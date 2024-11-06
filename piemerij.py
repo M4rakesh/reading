3ae0c2ed8953da04d1da85211185d30fff2bb55c
M4rakesh <maksims.rogozins@gmail.com>
2024-11-06 09:09:29 +0200

    1

---
+++ piemerij.py
@@ -0,0 +1,34 @@
+import json
+
+try:
+    with open("people_data.json","r",encoding='utf-8') as file:
+        people=json.load(file)
+except FileNotFoundError:
+    people=[]
+while True:
+    name=input("Ievadiet vārdu: ")
+    age=input("Ievadiet vecumu: ")
+    city=input("Ievadiet pilsetu: ")
+
+    people.append({
+        "name":name,
+        "age":age,
+        "city":city
+    })
+
+    another=input("Vai vēlaties pievienot vēl vienu cilvēku? (jā/nē)")
+    if another !="jā":
+        break
+with open("people_data.json","w",encoding='utf-8') as file:
+    json.dump(people,file,indent=4)
+
+    print("Dati ir veiksmīgi saglabāti JSON failā!")
+
+for person in people:
+    if int(person["age"]) >= 18:
+        print(f"Vārds:{person["name"]},Vecums:{person["age"]},Pilseta{person["city"]}")
+        
+    if int(person["city"][0]) =="R":
+        print(f"Pilsetas kura pirmais burts ir R:{people["city"]}")
+
+    
